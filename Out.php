d1b028de309164e22405275a10ffa2792a6069b7
alieneby <github@alien.de>
2020-09-23 14:33:33 +0200

    out fix

---
+++ Out.php
@@ -0,0 +1,67 @@
+<?php
+    require_once( 'OutMask.php' );
+    
+    class Out
+    {
+        /**
+         * List of template names. The order is important.
+         * @var array of strings (filenames)
+         */
+        static $_arrMasks = array();
+        
+        /**
+         * Container for values, which the application wants to display.
+         * @var associated array of strings
+         */
+        static $_arrVars = array();
+        
+        static $_arrIntern = array();
+        
+        /**
+         *
+         * @param String $strMasterMask mask name
+         */
+        static function addMask( $strMasterMask ) {
+            array_push( self::$_arrMasks, new OutMask( $strMasterMask ) );
+        }
+        
+        static function getMask( $strMask ) {
+            return empty( self::$_arrMasks[ $strMask ] ) ? null : self::$_arrMasks[ $strMask ];
+        }
+        
+        static function addVar( $strK, $strV ) {
+            self::$_arrVars[ $strK ] = $strV;
+        }
+        
+        static function getVar( $strK ) {
+            return empty( self::$_arrVars[ $strK ] ) ? '' : self::$_arrVars[ $strK ];
+        }
+        
+        private static function displayMask( OutMask $mask ) {
+            $nTime = microtime( true );
+            $sOut = "";
+            
+            if ( is_file( $t = Config::$_strAbsPath . "/output/default/" . $mask->strMask ) ) $sOut = $t;
+            
+            if ( $sOut ) {
+                self::$_arrIntern[ 'activeMask' ] = $sOut;
+                include $sOut;
+                unset( self::$_arrIntern[ 'activeMask' ] );
+                Log::always( "Out.display()", $sOut . ' in ' . round( ( microtime( true ) - $nTime ) * 1000, 2 ) . "ms" );
+            } else {
+                Log::systemError( $mask->strMask );
+            }
+        }
+        
+        static function display() {
+            foreach ( self::$_arrMasks as $mask ) self::displayMask( $mask );
+        }
+        
+        static public function displayAsString() {
+            ob_start();
+            Out::display();
+            $strHtml = ob_get_contents();
+            ob_end_clean();
+            return $strHtml;
+        }
+    }
